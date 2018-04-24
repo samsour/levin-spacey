769fba0afecbd854aeb7a6c12c3e3205476a055b
Sam Sauer <sam.sauer@hotmail.de>
2018-04-24 18:38:34 +0200

    player aim

--- src/assets/js/Player.js
+++ src/assets/js/Player.js
@@ -21,8 +21,18 @@ export default class Player extends GameObject {
         console.log("Player special!");
     }
 
+    calculateAimAngle(mouse) {
+        let mouseX = mouse.clientX - document.getElementById('js-main-canvas').getBoundingClientRect().left;
+        let mouseY = mouse.clientY - document.getElementById('js-main-canvas').getBoundingClientRect().top;
+
+        // mouseX -= player.x;
+        // mouseY -= player.y;
+
+        this.aimAngle = Math.atan2(mouseY,mouseX) / Math.PI * 180;
+    }
+
     initializeControls() {
-        document.onkeydown = (event) => {
+        document.onkeydown = event => {
             if(event.keyCode === 68)        //d
                 this.moving.right = true;
             else if(event.keyCode === 83)   //s
@@ -33,7 +43,7 @@ export default class Player extends GameObject {
                 this.moving.up = true;
         }
     
-        document.onkeyup = (event) => {
+        document.onkeyup = event => {
             if(event.keyCode === 68)        //d
                 this.moving.right = false;
             else if(event.keyCode === 83)   //s
@@ -44,25 +54,18 @@ export default class Player extends GameObject {
                 this.moving.up = false;
         }
 
-        document.onclick = (mouse) => {
+        document.onclick = mouse => {
             this.attack();
         }
     
-        document.oncontextmenu = (mouse) => {
+        document.oncontextmenu = mouse => {
             this.special();
             mouse.preventDefault();
         }
     
-        document.onmousemove = (mouse) => {
-            console.log("Mouse movement");
-
-            // var mouseX = mouse.clientX - document.getElementById('ctx').getBoundingClientRect().left;
-            // var mouseY = mouse.clientY - document.getElementById('ctx').getBoundingClientRect().top;
-    
-            // mouseX -= player.x;
-            // mouseY -= player.y;
-    
-            // player.aimAngle = Math.atan2(mouseY,mouseX) / Math.PI * 180;
+        document.onmousemove = mouse => {
+            this.calculateAimAngle(mouse);
+            console.log("Aim Angle:" + this.aimAngle);
         }
     }
 }
