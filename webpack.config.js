708be51864182b1951e5dbac9d79de7fec888580
samsour <sam.sauer@hotmail.de>
2019-08-07 17:41:44 +0200

    chore(build): remove jsx from loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader'
       },
