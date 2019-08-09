97f242a328dd89fa79b739d392f57bc5aa3212ba
samsour <sam.sauer@hotmail.de>
2019-08-10 00:57:53 +0200

    refactor(global): project structure

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,10 +1,37 @@
-// import Game from "./Game";
-// import Loader from "./Loader";
+import { connect, play } from './networking';
+import { startRendering, stopRendering } from './render';
+import { startCapturingInput, stopCapturingInput } from './input';
+import { downloadAssets } from './assets';
+import { initState } from './state';
+import { setLeaderboardHidden } from './leaderboard';
 
-// // Entry to the app. Loads all assets and creates the game
-// // Loader.load( 'localhost:6020', () => {
-// // 	new Game( document.body );
-// // });
-// document.addEventListener("DOMContentLoaded", event => {
-//   new Game(document.body);
-// });
+const playMenu = document.getElementById('play-menu');
+const playButton = document.getElementById('play-button');
+const usernameInput = document.getElementById('username-input');
+
+console.log(playButton);
+
+Promise.all([
+  connect(onGameOver),
+  downloadAssets(),
+]).then(() => {
+  playMenu.classList.remove('hidden');
+  usernameInput.focus();
+  playButton.onclick = () => {
+    // Play!
+    console.log('Start game!');
+    play(usernameInput.value);
+    playMenu.classList.add('hidden');
+    initState();
+    startCapturingInput();
+    startRendering();
+    setLeaderboardHidden(false);
+  };
+}).catch(console.error);
+
+function onGameOver() {
+  stopCapturingInput();
+  stopRendering();
+  playMenu.classList.remove('hidden');
+  setLeaderboardHidden(true);
+}
